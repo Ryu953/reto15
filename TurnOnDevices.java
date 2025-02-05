6f50cc3bffbffdbf0b68b1862c468897bb2e825c
Kevin <villegaskevin003@gmail.com>
2025-02-05 17:33:25 -0400

    reto15 codigo arreglo

--- TurnOnDevices.java
+++ TurnOnDevices.java
@@ -70,13 +70,13 @@ public class TurnOnDevices {
 
     public static void main(String[] args) {
 
-        //turnOnDevice(new Lamp());
-        //turnOnDevice(new Computer());
-        turnOnDevice(new CoffeMaKerAdaptor());
+        turnOnDevice(new Lamp());
+        turnOnDevice(new Computer());
+
     }
 
     private static void turnOnDevice(Connectable device) {
-        device.turnOff();
+        device.turnOn();
         System.out.println(device.isOn());
     }
 }
